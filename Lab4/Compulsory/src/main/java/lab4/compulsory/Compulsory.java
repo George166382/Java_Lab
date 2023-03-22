43fe8af9b1612c012495272463f6322feba9b17b
George Cojocaru <geocaru@gmail.com>
2023-03-22 17:00:34 +0200

    Lab4

--- Lab4/Compulsory/src/main/java/lab4/compulsory/Compulsory.java
+++ Lab4/Compulsory/src/main/java/lab4/compulsory/Compulsory.java
@@ -4,6 +4,15 @@
 
 package lab4.compulsory;
 
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import java.util.stream.IntStream;
+import lab4.compulsory.Student;
+
 /**
  *
  * @author G
@@ -11,6 +20,27 @@ package lab4.compulsory;
 public class Compulsory {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var students = IntStream.rangeClosed(0,3).mapToObj(i -> new Student("S" + i)).toArray(Student[]::new);
+        List<Student> listOfStudents = new LinkedList<>();
+        for(Student stud : students)
+        {
+            listOfStudents.add(stud);
+        }
+      listOfStudents.stream().sorted();
+        for(Student stud : listOfStudents)
+        {
+            System.out.print(stud.getName() + "\n");
+        }
+        var projects = IntStream.rangeClosed(0, 3).mapToObj(p -> new Project("P" + p)).toArray(Project[]::new);
+        Set<Project> setOfProjects = new TreeSet<>();
+        for(Project proj : projects)
+        {
+            setOfProjects.add(proj);
+        }
+        setOfProjects.stream().sorted();
+        for(Project proj : setOfProjects)
+        {
+            System.out.println(proj.getName());
+        }
     }
 }
